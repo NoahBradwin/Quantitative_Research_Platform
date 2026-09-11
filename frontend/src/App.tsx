72688b72ae368c027c9f0a431d7c3304ac9332d2
NoahBradwin <noahbradwin@gmail.com>
2026-09-11 19:59:12 -0400

    added file selector to load into table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,23 @@
-<input type="file" accept=".csv" />
+import type { ChangeEvent } from "react";
+
+async function handleFileChange(event: ChangeEvent<HTMLInputElement>) {
+  const file = event.currentTarget.files?.[0]
+
+  if (!file) {
+    return
+  }
+
+  const text = await file.text();
+  console.log(text);
+}
+
+function App() {
+  return (
+    <main>
+      <input type="file" accept=".csv" onChange={handleFileChange} />
+    </main>
+  )
+}
+
+export default App;
+
